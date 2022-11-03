92976752682969456efb8f8853abe75cfbb63353
Heitor Gomes <heitorgomes900@gmail.com>
2022-11-02 23:52:32 -0300

    update on restaurante resource

--- cadastro/src/main/java/com/github/heitorgomes900/ifood/cadastro/RestauranteResource.java
+++ cadastro/src/main/java/com/github/heitorgomes900/ifood/cadastro/RestauranteResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -17,5 +18,9 @@ public class RestauranteResource {
     public List<Restaurante> listarTodos() {
         return Restaurante.listAll();
     }
-    
+
+    @POST
+    public void adicionar(Restaurante dto) {
+
+    }
 }
